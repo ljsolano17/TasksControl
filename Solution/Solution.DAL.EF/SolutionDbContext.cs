c06c6fd4be4589557c32eb3a918f4dfc39ecc7ee
ljsolano17 <ljsolano17@hotmail.com>
2024-04-19 17:53:38 -0600

    Add Layers + DbContext

---
+++ Solution/Solution.DAL.EF/SolutionDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Solution.DO.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.DAL.EF
+{
+    public partial class SolutionDbContext : DbContext
+    {
+        public SolutionDbContext(DbContextOptions<SolutionDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Tasks> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tasks>(entity =>
+            {
+                entity.Property(e => e.Description).HasMaxLength(255);
+                entity.Property(e => e.DueDate).HasColumnType("datetime");
+                entity.Property(e => e.IsCompleted).HasDefaultValueSql("((0))");
+                entity.Property(e => e.Title).HasMaxLength(100);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+
+}
---
+++ Solution/Solution.DO/Objects/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.DO.Objects
+{
+    public class Tasks
+    {
+        public int TaskId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool? IsCompleted { get; set; }
+    }
+}
