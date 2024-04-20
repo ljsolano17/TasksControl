0730780e1aeaaaea39f1a06dc00f22d29d32a3b3
ljsolano17 <ljsolano17@hotmail.com>
2024-04-20 01:20:24 -0600

    Add Controller structure

---
+++ Solution/Solution.API.Tasks/Controllers/TasksController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Solution.DAL.EF;
+using System.Reflection.Metadata.Ecma335;
+using data = Solution.DO.Objects;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Solution.API.Tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private SolutionDbContext _context;
+        public TasksController(SolutionDbContext context) 
+        {
+            this._context = context;
+        }
+        // GET: api/<TasksController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<data.Tasks>>> GetAll()
+        {
+            return  new BS.Tasks(_context).GetAll().ToList();
+        }
+
+        // GET api/<TasksController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TasksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TasksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TasksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
